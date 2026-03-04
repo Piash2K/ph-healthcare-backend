3ba3b96f0abb95a0aad99a7b5fcaa33f9037195f
Piash Islam <piashislam2k@gmail.com>
2026-03-04 22:09:18 +0600

    setting up auth and creating auth schemas

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { SpecialtyRoute } from "../modules/specialty/specialty.route";
+import { AuthRoutes } from "../modules/auth/auth.router";
 
 const router = Router();
 
+router.use("/auth", AuthRoutes);
 router.use("/specialties", SpecialtyRoute);
 
 export const IndexRoutes = router;

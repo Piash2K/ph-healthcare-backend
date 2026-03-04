e9663cb0d2a72f6a79f947165140d9a643fd419f
Piash Islam <piashislam2k@gmail.com>
2026-03-04 22:58:48 +0600

    login user with email password functionalites added done

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,7 +1,13 @@
+import { UserStatus } from "../../../generated/prisma/enums";
 import { auth } from "../../lib/auth";
 
 interface IRegister {
   name: string;
+  email: string;
+  password: string;
+}
+
+interface ILogin {
   email: string;
   password: string;
 }
@@ -21,6 +27,24 @@ const registerPatient = async (payload: IRegister) => {
   return data;
 };
 
+const loginUser = async (payload: ILogin) => {
+  const { email, password } = payload;
+  const data = await auth.api.signInEmail({
+    body: {
+      email,
+      password,
+    },
+  });
+  if (data.user.status===UserStatus.BLOCKED) {
+    throw new Error("Your account is blocked. Please contact support.");
+  }
+  if(data.user.isDeleted||data.user.status===UserStatus.DELETED){
+    throw new Error("Your account is deleted. Please contact support.");
+  }
+  return data.user;
+};
+
 export const AuthService = {
   registerPatient,
+  loginUser,    
 };

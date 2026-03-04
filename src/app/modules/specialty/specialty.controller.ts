f8f91f0c9f94e81ec0cafff67a6beaee5a41c43f
Piash Islam <piashislam2k@gmail.com>
2026-03-04 16:11:50 +0600

    creating send response ultility function

--- src/app/modules/specialty/specialty.controller.ts
+++ src/app/modules/specialty/specialty.controller.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";
 import { SpecialtyService } from "./specialty.service";
 import { catchAsync } from "../../shared/catchAsync";
+import { sendResponse } from "../../shared/sendResponse";
 
 const createSpecialty = catchAsync(async (req: Request, res: Response) => {
   const result = await SpecialtyService.createSpecialty(req.body);
-  res.status(201).json({
+  sendResponse(res, {
+    httpStatusCode: 201,
     success: true,
     message: "Specialty created successfully",
     data: result,
@@ -13,7 +15,8 @@ const createSpecialty = catchAsync(async (req: Request, res: Response) => {
 
 const getAllSpecialties = catchAsync(async (req: Request, res: Response) => {
   const result = await SpecialtyService.getAllSpecialties();
-  res.status(200).json({
+  sendResponse(res, {
+    httpStatusCode: 200,
     success: true,
     message: "Specialties fetched successfully",
     data: result,
@@ -23,7 +26,8 @@ const getAllSpecialties = catchAsync(async (req: Request, res: Response) => {
 const deleteSpecialty = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
   const result = await SpecialtyService.deleteSpecialty(id as string);
-  res.status(200).json({
+  sendResponse(res, {
+    httpStatusCode: 200,
     success: true,
     message: "Specialty deleted successfully",
     data: result,
@@ -33,9 +37,10 @@ const deleteSpecialty = catchAsync(async (req: Request, res: Response) => {
 const updateSpecialty = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
   const result = await SpecialtyService.updateSpecialty(id as string, req.body);
-  res.status(201).json({
+  sendResponse(res, {
+    httpStatusCode: 201,
     success: true,
-    message: "Specialty update successfully",
+    message: "Specialty updated successfully",
     data: result,
   });
 });

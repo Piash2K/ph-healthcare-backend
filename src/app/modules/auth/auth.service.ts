7eb15dd298343037784d432aa3827189de4b2350
Piash Islam <piashislam2k@gmail.com>
2026-03-08 21:16:46 +0600

    handle error for transaction when patient register

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -24,20 +24,30 @@ const registerPatient = async (payload: IRegister) => {
   if (!data.user) {
     throw new Error("Failed to register user");
   }
-  const patient = await prisma.$transaction(async (tx) => {
-    const patientTx = await tx.patient.create({
-      data: {
-        userId: data.user.id,
-        name: payload.name,
-        email: payload.email,
+  try {
+    const patient = await prisma.$transaction(async (tx) => {
+      const patientTx = await tx.patient.create({
+        data: {
+          userId: data.user.id,
+          name: payload.name,
+          email: payload.email,
+        },
+      });
+      return patientTx;
+    });
+    return {
+      ...data,
+      patient,
+    };
+  } catch (error) {
+    console.log("Transaction error:", error);
+    await prisma.user.delete({
+      where: {
+        id: data.user.id,
       },
     });
-    return patientTx;
-  });
-  return {
-    ...data,
-    patient,
-  };
+    throw error;
+  }
 };
 
 const loginUser = async (payload: ILogin) => {

f4d87ada9fbc0bad5d462bbd068d730c971205fd
Piash Islam <piashislam2k@gmail.com>
2026-03-09 22:20:00 +0600

    added route, controller, service business logic with transaction in user service

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { SpecialtyRoute } from "../modules/specialty/specialty.route";
 import { AuthRoutes } from "../modules/auth/auth.router";
+import { UserRoutes } from "../modules/user/user.route";
 
 const router = Router();
 
 router.use("/auth", AuthRoutes);
 router.use("/specialties", SpecialtyRoute);
+router.use("/users", UserRoutes);
 
 export const IndexRoutes = router;

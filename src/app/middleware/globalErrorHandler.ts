f8288b183a4d790b0e0120638cb388a149e04fcf
Piash Islam <piashislam2k@gmail.com>
2026-04-21 22:40:55 +0600

    creating zod error handler for global error handler

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -3,6 +3,13 @@
 import { NextFunction, Request, Response } from "express";
 import { envVars } from "../config/env";
 import status from "http-status";
+import z, { ZodError } from "zod";
+import { error } from "node:console";
+
+interface TErrorSource {
+  path: string;
+  message: string;
+}
 
 export const globalErrorHandler = (
   err: any,
@@ -13,12 +20,30 @@ export const globalErrorHandler = (
   if (envVars.NODE_ENV === "development") {
     console.log("Error from global error handler:", err);
   }
-  const statusCode: number = status.INTERNAL_SERVER_ERROR;
-  const message: string = "Internal Server Error";
-  console.log(err);
+
+  const errorSources: TErrorSource[] = [];
+  let statusCode: number = status.INTERNAL_SERVER_ERROR;
+  let message: string = "Internal Server Error";
+
+  if (err instanceof z.ZodError) {
+    statusCode = status.BAD_REQUEST;
+    message = "Zod Validation Error";
+
+    err.issues.forEach((issue) => {
+      errorSources.push({
+        path:
+          issue.path.length > 1
+            ? issue.path.join("=>")
+            : issue.path[0].toString(),
+        message: issue.message,
+      });
+    });
+  }
+
   res.status(statusCode).json({
     success: false,
     message: message,
-    error: err.message,
+    errorSources,
+    error: envVars.NODE_ENV === "development" ? err : undefined,
   });
 };
